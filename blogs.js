9faf9abdb4f999e68523dc47bdcb37b6cc3c1fa1
Nayan <kunayan@deloitte.com>
2023-03-17 21:14:06 +0530

    my first

--- blogs.js
+++ blogs.js
@@ -1,13 +1,14 @@
 const url = 'https://jsonplaceholder.typicode.com/posts';
 $(document).ready(function () {
     $.get(url, function (data) {
-        data.forEach(element => {
-                document.getElementsByClassName('content').innerHTML=console.log(element.title+"<br>");
+        // data.forEach(element => {
+            for(i=0;i<)
+                // document.getElementsByClassName('content').innerHTML=console.log(' ' +element.title+"<br>");
             // $(".content").text(element.title);
 
-            Object.entries(element).forEach(([key,value])=>{
-                $(".content").text(value);
-            });
+            // Object.entries(element).forEach(hi = ([key,value])=>{
+            //     $(".content").text(value);
+            // });
                       
         });
     })
